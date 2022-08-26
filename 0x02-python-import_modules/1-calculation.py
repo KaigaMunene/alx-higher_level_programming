9a789d140618077755a11de52829fae69547ba4c
KaigaMunene <kaigamunene@gmail.com>
2022-08-26 04:59:08 +0300

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, mutiplication, division
-
-if (__name__ == "__main__"):
+if __name__ == "__main__":
+    from calculator_1 import add, subtract, mutiplication, division
     a = 10
     b = 5
-
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, subtract(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mutiplication(a, b)))
