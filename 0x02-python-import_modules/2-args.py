3b29a674f9a199c84bf0b964dff035363c653f28
KaigaMunene <kaigamunene@gmail.com>
2022-08-26 05:22:19 +0300

    fix error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
+from sys import argv
+
+if (__name__ == "__main__"):
     arg_num = len(argv) - 1
+
     print("{:d} argument{:s}{:s}".format(arg_num, "" if arg_num == 1 else
-                                         "s", "." if arg_num == 1 else ":"))
+                                         "s", "." if arg_num < 1 else ":"))
     for counter, value in enumerate(argv[1:], 1):
         print("{:d}: {:s}".format(counter, value))
