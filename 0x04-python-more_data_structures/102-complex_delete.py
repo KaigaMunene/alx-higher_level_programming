f1bc7338f1dc922818621a7236b3b822ae0d0b3d
KaigaMunene <kaigamunene@gmail.com>
2022-09-05 15:07:09 +0300

    task 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    my_keys = a_dictionary.keys()
+    for i in my_keys:
+        if value == a_dictionary.get(i):
+            del a_dictionary(i)
+
+    return (a_dictionary)
