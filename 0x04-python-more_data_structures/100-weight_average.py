795e7f0258f2318af5862ae82d2402e50a78b13c
KaigaMunene <kaigamunene@gmail.com>
2022-09-05 14:42:43 +0300

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+def weight_average(my_list=[]):
+    top = bottom = 0
+
+    if (my_list is None or my_list == []):
+        return 0
+
+    for x, y in my_list:
+        top += x * y
+        bottom += y
+    return (top/bottom)
