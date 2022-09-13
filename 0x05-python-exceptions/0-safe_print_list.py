0cc1c97915d3f93055bcd9eaae507b2a8f8bc3a0
KaigaMunene <kaigamunene@gmail.com>
2022-09-13 18:40:52 +0300

    fix error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0 ):
     while count < x:
         try:
             print("{}".format(my_list[count]), end="")
-        except:
+        except Exception:
             print()
             return (count)
         count += 1
