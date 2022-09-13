f75f2171956b3cf6fa7f591aee3d63c9ab25dce5
KaigaMunene <kaigamunene@gmail.com>
2022-09-13 18:25:23 +0300

    task 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0 ):
+    count = 0
+    while count < x:
+        try:
+            print(next(my_list))
+        except StopIteration:
+            print("You have reached the end!")
+            break
+        count += 1
