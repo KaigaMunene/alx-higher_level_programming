1087c14b98ecb5317b12ffa2cf45b25f183134fd
KaigaMunene <kaigamunene@gmail.com>
2022-09-15 05:50:21 +0300

    fix error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,10 +3,14 @@
 
 
 class Square:
-    """ This class defines a square object """
-    def __init__(self, size=0):
-        """ Constructor for the Square class"""
-        self.__size = size
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -21,8 +25,22 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """Get/set the current position of the square."""
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
-        """Returns the current area of the square."""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
