453d9ea3af94961a55414b87e60a5951492f512d
KaigaMunene <kaigamunene@gmail.com>
2022-09-15 05:00:02 +0300

    fix error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    while count < x:
+    for i in range(x):
         try:
             print("{:d}".format(my_list[count]), end="")
             count += 1
