66555563c6099c5b80ca4babe754eaead7953a66
KaigaMunene <kaigamunene@gmail.com>
2022-09-21 13:15:34 +0300

    fix linter error

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -63,7 +63,7 @@ class Rectangle:
             if i != self.__height - 1:
                 shape.append("\n")
         return ("".join(shape))
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """ Returns the biggest rectangle based on the area """
@@ -71,8 +71,8 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle ")
         if not isinstance(rect_2):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() == rect_2.area():
-            return  rect_1
+        if rect_1.area() >= rect_2.area():
+            return rect_1
         return rect_2
 
     def __repr__(self):
