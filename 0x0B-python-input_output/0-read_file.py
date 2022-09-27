be08a20e214a2622cdd07373ce0cc8a40b141a36
KaigaMunene <kaigamunene@gmail.com>
2022-09-27 10:41:37 +0300

    task 0 read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/pyhton3
+"""
+Module content
+
+0-read_file - contains function to read UTF8 text filles and print to stdout
+"""
+
+
 def read_file(filename=""):
-    with open("UTF8.txt", encoding="utf-8") as f:
-        f.read()
+    """ Function that reads a text file and prints it to stdout """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
