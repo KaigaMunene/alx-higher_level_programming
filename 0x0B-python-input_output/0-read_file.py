b3f88207a4b111f442edf94fae007f793a07f8f4
KaigaMunene <kaigamunene@gmail.com>
2022-09-27 12:33:46 +0300

    fix error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,3 @@
-#!/usr/bin/pyhton3
 #!/usr/bin/python3
 """
 Module content
@@ -9,4 +8,4 @@ Module content
 def read_file(filename=""):
     """ Read UTF8 text files and print content to stdout """
     with open(filename, "r", encoding="utf-8") as f:
-            print(f.read(), end="")
+        print(f.read(), end="")
