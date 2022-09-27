4386e21a2addf8dfe8bd15f16185996d22bdcfbe
KaigaMunene <kaigamunene@gmail.com>
2022-09-27 13:38:47 +0300

    task 9 student to json

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module content
+Create a 'Student' class with public attributes and methods
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Return dictionary content of this Student object """
+        return (self.__dict__)
