9dab1f3ece6c87b581aac76ad8c34b16cdca39ff
KaigaMunene <kaigamunene@gmail.com>
2022-10-03 14:29:57 +0300

    update task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -10,7 +10,51 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """ initialize contructor method """
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Getter for 'width' attribute """
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """ Setter for 'width attribute """
+        self.validate("width", width, 0)
         self.__width = width
+
+    @property
+    def height(self):
+        """ Getter for 'height' attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """ Setter for 'height' attribute """
+        self.validate("height", height, 0)
         self.__height = height
+
+    @property
+    def x(self):
+        """ Getter for 'x' attribute """
+        return self.__x
+    
+    @x.setter
+    def x(self, x):
+        """ Setter for 'x' attribute """
+        self.validate("x", x, 0)
         self.__x = x
+    
+    @property
+    def y(self):
+        """ Getter for 'y' attribute """
+        return self.__y
+    
+    @y.setter
+    def y(self, y):
+        """ Setter for 'y' attribute """
+        self.validate("y", y, 0)
         self.__y = y
