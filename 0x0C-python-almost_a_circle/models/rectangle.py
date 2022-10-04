7e4f29d80090beae736f2d9eb28e81358d8ad9e7
KaigaMunene <kaigamunene@gmail.com>
2022-10-04 10:51:52 +0300

    fix checker errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,11 +9,12 @@ class Rectangle(Base):
     """ rectangle inherits from base """
     def __init__(self, width, height, x=0, y=0, id=None):
         """ initialize contructor method """
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
+
 
     @property
     def width(self):
