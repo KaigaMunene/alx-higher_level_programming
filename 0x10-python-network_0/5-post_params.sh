e89e9708c92235c68af725706db9a8a1b6a5c55b
KaigaMunene <kaigamunene@gmail.com>
2022-12-20 17:36:18 +0300

    update task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send POST request to passed URL and display body of response
-curl -X POST -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+curl -X POST -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
