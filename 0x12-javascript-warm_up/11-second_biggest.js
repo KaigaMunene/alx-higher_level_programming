55238649c681636375446f3e7129e3b71072affa
KaigaMunene <kaigamunene@gmail.com>
2023-03-15 05:38:00 +0300

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+const { argv } = require('process');
 
-const nums = process.argv.slice(2).sort();
-nums.length > 1 ? console.log(nums[nums.length - 2]) : console.log(0);
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  let newArgv = argv.slice(2, argv.length);
+  newArgv = newArgv.map((value) => parseInt(value));
+  newArgv.sort((a, b) => b - a);
+  console.log(newArgv[1]);
+}
