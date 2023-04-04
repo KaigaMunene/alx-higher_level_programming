343e21a640d6f74ee250ed1ff288c935ae8455ab
KaigaMunene <kaigamunene@gmail.com>
2023-04-05 02:54:42 +0300

    update task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" Fetch response from URL using urllib package """
-
-from urllib import request
+"""Fetches https://intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
-    """ Make a request to https://intranet.hbtn.io/status """
-    with request.urlopen('http://0.0.0.0:5050/status') as response:
-        html = response.read()
+    import urllib.request
 
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf8")))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
