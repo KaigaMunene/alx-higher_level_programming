e3d14af514ef6e7ab2d9b004ac645268191fb9ae
KaigaMunene <kaigamunene@gmail.com>
2023-04-26 02:18:28 +0300

    advanced task 2

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+
+request(url, async (error, response, body) => {
+  if (error) { console.log(error); }
+  const result = JSON.parse(body);
+
+  for (const charURL of result.characters) {
+    await new Promise((resolve, reject) => {
+      request(charURL, (err, r, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
+    });
   }
 });
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
-}
